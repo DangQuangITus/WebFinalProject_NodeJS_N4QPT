be7837340aad158f5fca9645c015d12671aa16f7
Duy Phuong <tranduyphuong20100@gmail.com>
2018-06-28 17:54:53 +0700

    delete

--- project/repos/customerRepo.js
+++
@@ -1,6 +0,0 @@
-var db = require('../fn/db');
-
-exports.update = user => {
-   var sql = "UPDATE `users` SET `f_Username`='"+user.username+"', `f_Password`='"+user.password+"', `f_Name`='"+user.name+"', `f_Email`='"+user.email+"', `f_DOB`='"+user.dob+"' WHERE `f_Id`='"+user.Id+"';";
-     return db.save(sql);
-}
